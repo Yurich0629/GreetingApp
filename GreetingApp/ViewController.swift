4cd8ff54198b388479bba24c151c700cf718e998
Ilya Desiatow <140006151+Yurich0629@users.noreply.github.com>
2023-09-14 18:13:01 +0300

    logics for project was finished

--- GreetingApp/ViewController.swift
+++ GreetingApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var showGreetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        greetingLabel.isHidden.toggle()
+        showGreetingButton.layer.cornerRadius = 10
     }
 
 
+    @IBAction func showGreetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        showGreetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+    }
+    
 }
 
